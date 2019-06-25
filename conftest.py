dddf03591cc5d09a0f9299f566cb6cfa5372f18a
ZhivatsNik <zhivats@gmail.com>
2019-06-25 14:10:27 +0300

    Updated files conftest.py and test_items.py

--- conftest.py
+++ conftest.py
@@ -2,38 +2,22 @@ import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
-options = Options()
-options.add_experimental_option(
-    'prefs', {'intl.accept_languages': "es"})
-browser = webdriver.Chrome(options=options)
-
-language = request.config.getoption("language")
-
+# функция определяет необходимость вводить параметр language в командной строке при запуске теста
 def pytest_addoption(parser):
     parser.addoption('--language', action='store', default=None,
-                     help="Choose browser language: en, es")
+                     help="Choose browser language: en, es, fr")
 
-
+# функция определяет браузер какой локализации запускать
 @pytest.fixture(scope="function")
 def browser(request):
     language = request.config.getoption("language")
-    if language == "es":
-        print("\nstart chrome browser loc. 'es' for test..")
-        browser = webdriver.Chrome()
-    elif language == "en":
-        print("\nstart firefox browser loc 'en' for test..")
-        browser = webdriver.Firefox()
-    else:
-        print("Browser language {} still is not implemented".format(language))
+    options = Options()
+    options.add_experimental_option(
+        'prefs', {'intl.accept_languages': language})
+    browser = webdriver.Chrome(options=options)
     yield browser
     print("\nquit browser..")
     browser.quit()
 
 
-#@pytest.fixture(scope="function")
-#def browser():
-#    print("\nstart browser for test..")
-#    browser = webdriver.Chrome()
-#    yield browser
-#    print("\nquit browser..")
-#    browser.quit()
+
--- test_items.py
+++ test_items.py
@@ -1,10 +1,12 @@
 
+import time
+
 link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
 
-language = request.config.getoption("language")
-
-def get_basket_button():
+# функция вызывает фикстуру browser как параметр
+def test_get_basket_button(browser):
     browser.get(link)
-    add_to_basket_button = browser.find_element_by_css_selector("#add_to_basket_form button").text
-    assert add_to_basket_button == 'Add to basket', f'Кнопка содержит текст {add_to_basket_button}'
-
+# указал 10 секунд времени, при 30 можно свихнуться:)
+    time.sleep(10)
+    browser.find_element_by_css_selector("#add_to_basket_form button")
+    assert True
